2616d316fca6887f84a5486eb5d6860d4524325d
aiswaryalakshmia <aiswaryaapnr@gmail.com>
2026-06-28 13:54:18 +0530

    week4 pendings

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import redirect, render
 from django.views.decorators.cache import never_cache
-from apps.products.models import Category
+from apps.products.models import Category,ProductVariant
+
 
 @never_cache
 def home(request):
-    categories = Category.objects.filter(status='active')
     if request.user.is_superuser:
         return redirect('admin_dashboard')
-    return render(request, 'home.html',{
-        'categories':categories
+
+    categories = Category.objects.filter(status='active')
+
+    new_arrivals = ProductVariant.objects.filter(
+        status='active',
+        product__status='active',
+        product__category__status='active',
+        stock__gt=0
+    ).select_related(
+        'product',
+        'product__category'
+    ).prefetch_related(
+        'images'
+    ).order_by('-created_at')[:4]
+
+    return render(request, 'home.html', {
+        'categories': categories,
+        'new_arrivals': new_arrivals,
     })

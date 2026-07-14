e36ab9f7b0f6c05dae7eaeb15e16dcb6ad2e3522
aiswaryalakshmia <aiswaryaapnr@gmail.com>
2026-07-15 01:21:13 +0530

    feat:error handling

--- apps/common/decorators.py
+++ apps/common/decorators.py
@@ -1,14 +1,17 @@
 from django.shortcuts import redirect
+from django.contrib import messages
 
 def admin_required(view_func):
 
     def wrapper(request, *args, **kwargs):
 
         if not request.user.is_authenticated:
-            return redirect('login')
+            # messages.error(request, "Please log in as admin to continue.")
+            return redirect('admin_login')
 
         if not request.user.is_superuser:
-            return redirect('home')
+            # messages.error(request, "You do not have permission to access the admin panel.")
+            return redirect('admin_login')
 
         return view_func(request, *args, **kwargs)
 
--- scentora/settings.py
+++ scentora/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'scentora.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
